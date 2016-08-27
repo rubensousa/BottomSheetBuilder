b6dac46fdb7180f27991958211e38c68b03ce687
ruben <rubensousa.mieti@gmail.com>
2016-08-27 13:27:02 +0100

    Remove workarounds since the latest support library fixed some bugs

--- library/src/main/java/com/github/rubensousa/bottomsheetbuilder/BottomSheetMenuDialog.java
+++ library/src/main/java/com/github/rubensousa/bottomsheetbuilder/BottomSheetMenuDialog.java
@@ -4,9 +4,8 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
-import android.support.design.widget.CoordinatorLayout;
-import android.util.TypedValue;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.github.rubensousa.bottomsheetbuilder.adapter.BottomSheetItemClickListener;
 import com.github.rubensousa.bottomsheetbuilder.adapter.BottomSheetMenuItem;
@@ -25,11 +24,6 @@ public class BottomSheetMenuDialog extends BottomSheetDialog implements BottomSh
         super(context, theme);
     }
 
-    @Override
-    public void setContentView(View view) {
-        getDelegate().setContentView(wrapInBottomSheet(view));
-    }
-
     public void setBottomSheetCallback(BottomSheetBehavior.BottomSheetCallback callback) {
         mCallback = callback;
     }
@@ -42,47 +36,14 @@ public class BottomSheetMenuDialog extends BottomSheetDialog implements BottomSh
         return mBehavior;
     }
 
-    private View wrapInBottomSheet(View view) {
-        final CoordinatorLayout coordinator = (CoordinatorLayout) View.inflate(getContext(),
-                R.layout.bottomsheetbuilder_dialog, null);
-
-        CoordinatorLayout.LayoutParams layoutParams
-                = new CoordinatorLayout.LayoutParams(CoordinatorLayout.LayoutParams.MATCH_PARENT,
-                CoordinatorLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.setBehavior(new BottomSheetBehavior());
-        coordinator.addView(view, layoutParams);
-        mBehavior = BottomSheetBehavior.from(view);
-        mBehavior.setBottomSheetCallback(mBottomSheetCallback);
-        mBehavior.setHideable(true);
-
-        if (shouldWindowCloseOnTouchOutside()) {
-            coordinator.findViewById(R.id.touch_outside)
-                    .setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            if (isShowing()) {
-                                cancel();
-                            }
-                        }
-                    });
-        }
-        return coordinator;
-    }
-
     @Override
     protected void onStart() {
         super.onStart();
-        if (mBehavior != null) {
-            mBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        FrameLayout sheet = (FrameLayout) findViewById(R.id.design_bottom_sheet);
+        if (sheet != null) {
+            mBehavior = BottomSheetBehavior.from(sheet);
+            mBehavior.setBottomSheetCallback(mBottomSheetCallback);
         }
-    }
-
-    private boolean shouldWindowCloseOnTouchOutside() {
-        TypedValue value = new TypedValue();
-
-        return getContext().getTheme()
-                .resolveAttribute(android.R.attr.windowCloseOnTouchOutside, value, true)
-                && value.data != 0;
     }
 
     private BottomSheetBehavior.BottomSheetCallback mBottomSheetCallback
@@ -95,8 +56,7 @@ public class BottomSheetMenuDialog extends BottomSheetDialog implements BottomSh
                 mCallback.onStateChanged(bottomSheet, newState);
             }
 
-            if (newState == BottomSheetBehavior.STATE_HIDDEN
-                    || newState == BottomSheetBehavior.STATE_COLLAPSED) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
                 dismiss();
             }
         }
