89a7ac84c5529080d7a7677b8e5a74c0c5a9af73
ruben <rubensousa.mieti@gmail.com>
2018-04-05 21:45:54 +0100

    Update dependencies

--- sample/src/main/java/com/github/rubensousa/bottomsheetbuilder/sample/MainActivity.java
+++ sample/src/main/java/com/github/rubensousa/bottomsheetbuilder/sample/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
